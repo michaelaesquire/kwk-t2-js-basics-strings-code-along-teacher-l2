1f10f7bdf9d37bc1277b34199a229b5f9676512f
Michaela Olson <mco44@cornell.edu>
2018-05-08 20:58:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const currentUser = "Popsicle";
 let welcomeMessage = "Welcome to Flatbook, ";
 welcomeMessage = welcomeMessage + currentUser + "!";
 let excitedWelcomeMessage = "WELCOME TO FLATBOOK, ";
-excitedWelcomeMessage = excitedWelcomeMessage + toUpperCase(currentUser) + "!";
+excitedWelcomeMessage = excitedWelcomeMessage + str.toUpperCase(currentUser) + "!";
