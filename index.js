980b8bae882209dcf1171d27cfc8c230a2026612
Michaela Olson <mco44@cornell.edu>
2018-05-08 21:00:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = "Popsicle";
+let welcomeMessage = "Welcome to Flatbook, ";
+welcomeMessage = welcomeMessage + currentUser + "!";
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, ";
+excitedWelcomeMessage = excitedWelcomeMessage + currentUser.toUpperCase() + "!";
+let shortGreeting = "Welcome, ";
+shortGreeting = shortGreeting + currentUser.charAt(0) + "!";
