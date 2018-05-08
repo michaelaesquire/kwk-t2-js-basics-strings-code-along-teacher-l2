a5b08a6d9d077edf09dde520194e18b76026c630
Michaela Olson <mco44@cornell.edu>
2018-05-08 20:56:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = "Popsicle";
 let welcomeMessage = "Welcome to Flatbook, ";
-welcomeMessage = welcomeMessage + currentUser;
+welcomeMessage = welcomeMessage + currentUser + "!";
